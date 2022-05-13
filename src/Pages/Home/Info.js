5198de8d2226bfeb1cb504a262d56f4f7e284145
AbuZobayed <zobayed.ict05@gmail.com>
2022-05-14 00:15:54 +0600

    added footer

--- src/Pages/Home/Info.js
+++ src/Pages/Home/Info.js
@@ -7,9 +7,21 @@ import phone from "../../assets/icons/phone.svg";
 const Info = () => {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-3 gap-5">
-      <InfoCard cardTitle="Opening Hours" bgClass="bg-gradient-to-r from-secondary to-primary" img={marker}></InfoCard>
-      <InfoCard cardTitle="Our Locations" bgClass="bg-neutral" img={phone}></InfoCard>
-      <InfoCard cardTitle="Contact Us" bgClass="bg-gradient-to-r from-secondary to-primary" img={clock}></InfoCard>
+      <InfoCard
+        cardTitle="Opening Hours"
+        bgClass="bg-gradient-to-r from-secondary to-primary"
+        img={marker}
+      ></InfoCard>
+      <InfoCard
+        cardTitle="Our Locations"
+        bgClass="bg-neutral"
+        img={phone}
+      ></InfoCard>
+      <InfoCard
+        cardTitle="Contact Us"
+        bgClass="bg-gradient-to-r from-secondary to-primary"
+        img={clock}
+      ></InfoCard>
     </div>
   );
 };
