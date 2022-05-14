e0d37b661802da415d26bce7f11863f33d446e0c
AbuZobayed <zobayed.ict05@gmail.com>
2022-05-14 09:20:02 +0600

    create route appointment page

--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -55,9 +55,9 @@ const Navbar = () => {
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal p-0">
-          <li>
+        {/* <li>
             <a>Home</a>
-          </li>
+          </li> */}
           {menuitems}
         </ul>
       </div>
