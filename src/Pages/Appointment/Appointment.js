82adfcfc37cc393a8ba1f9e6711f7febceafcefc
AbuZobayed <zobayed.ict05@gmail.com>
2022-05-14 11:30:58 +0600

    added serice component

--- src/Pages/Appointment/Appointment.js
+++ src/Pages/Appointment/Appointment.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import Footer from '../Shared/Footer';
-import AppointmentBanner from './AppointmentBanner';
-
+import React, { useState } from "react";
+import Footer from "../Shared/Footer";
+import AppointmentBanner from "./AppointmentBanner";
+import AvailableAppointments from "./AvailableAppointments";
 
 const Appointment = () => {
-    return (
-        <div>
-            <AppointmentBanner></AppointmentBanner>
-            <Footer></Footer>
-        </div>
-    );
+    const [date, setDate] = useState(new Date());
+  return (
+    <div>
+      <AppointmentBanner date={date} setDate={setDate}></AppointmentBanner>
+      <AvailableAppointments date={date}></AvailableAppointments>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default Appointment;
