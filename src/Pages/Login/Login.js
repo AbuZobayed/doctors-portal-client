725c9c6c419f3c0c581d2cf39713e346a5b214ae
AbuZobayed <zobayed.ict05@gmail.com>
2022-05-15 20:49:56 +0600

    implement registration form to create new user

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -3,6 +3,7 @@ import { useSignInWithEmailAndPassword, useSignInWithGoogle } from "react-fireba
 import auth from "../../firebase.init";
 import { useForm } from "react-hook-form";
 import Loading from "../Shared/Loading";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth);
@@ -24,8 +25,8 @@ const Login = () => {
     signError=<p className="text-red-500"><small>{error?.message || gError?.message}</small></p>
   }
 
-  if (gUser) {
-    console.log(gUser);
+  if (user || gUser) {
+    console.log(user || gUser);
   }
 
   const onSubmit = (data) => {
@@ -93,6 +94,7 @@ const Login = () => {
                 {signError}
             <input className="btn w-full max-w-xs text-white" type="submit" value='Login' />
           </form>
+          <p><small>New to Doctors Portal <Link className="text-secondary" to="/signup">Create New Account</Link></small></p>
           <div className="divider">OR</div>
           <button
             onClick={() => signInWithGoogle()}
