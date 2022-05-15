be960816ed36aab21630238f8be50bb8209f06ed
AbuZobayed <zobayed.ict05@gmail.com>
2022-05-15 21:18:08 +0600

    update user name and implement conditional sign out

--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -1,7 +1,18 @@
 import React from "react";
+import {signOut} from 'firebase/auth';
+import { useAuthState } from "react-firebase-hooks/auth";
 import { Link } from "react-router-dom";
+import auth from "../../firebase.init";
 
 const Navbar = () => {
+
+  const [user, loading, error] = useAuthState(auth);
+
+  const logout = () => {
+    signOut(auth);
+  };
+
+
   const menuitems = (
     <>
       <li>
@@ -20,7 +31,9 @@ const Navbar = () => {
         <Link to="/about">About</Link>
       </li>
       <li>
-        <Link to="/login">Login</Link>
+        {user ? <button class="btn btn-ghost" onClick={logout}>Sign Out</button> :
+          <Link to="/login">Login</Link>
+          }
       </li>
     </>
   );
