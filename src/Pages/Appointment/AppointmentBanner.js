a2c023fc5be6535843073030b363e6f820e4065d
AbuZobayed <zobayed.ict05@gmail.com>
2022-05-15 11:38:37 +0600

    google sign method done

--- src/Pages/Appointment/AppointmentBanner.js
+++ src/Pages/Appointment/AppointmentBanner.js
@@ -1,29 +1,26 @@
 import React from "react";
 import chair from "../../assets/images/chair.png";
-import bg from '../../assets/images/bg.png'
+import bg from "../../assets/images/bg.png";
 import { DayPicker } from "react-day-picker";
 import "react-day-picker/dist/style.css";
 
-
-const AppointmentBanner = ({date , setDate}) => {
-  
+const AppointmentBanner = ({ date, setDate }) => {
   return (
-    <div style={{
+    <div
+      style={{
         background: `url(${bg})`,
-        backgroundSize: 'cover'
-    }}
-    class="hero min-h-screen">
-      <div class="hero-content flex-col lg:flex-row-reverse">
+        backgroundSize: "cover",
+      }}
+      className="hero min-h-screen"
+    >
+      <div className="hero-content flex-col lg:flex-row-reverse">
         <img
           src={chair}
-          class="max-w-sm rounded-lg shadow-2xl"
+          className="max-w-sm rounded-lg shadow-2xl"
           alt="Dentist chair"
         />
         <div>
-          <DayPicker 
-          mode="single" 
-          selected={date} 
-          onSelect={setDate} />
+          <DayPicker mode="single" selected={date} onSelect={setDate} />
         </div>
       </div>
     </div>
