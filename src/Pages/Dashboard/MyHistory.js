e0e2de3ab1c222771f29f1192af3a49f1831be94
AbuZobayed <zobayed.ict05@gmail.com>
2022-05-16 22:40:47 +0600

    issue simple jwt token and store it in the local storage

---
+++ src/Pages/Dashboard/MyHistory.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MyHistory = () => {
+    return (
+        <div>
+            <h2>This is my Appointment history!!</h2>
+        </div>
+    );
+};
+
+export default MyHistory;
--- src/Pages/Login/SignUp.js
+++ src/Pages/Login/SignUp.js
@@ -8,6 +8,7 @@ import auth from "../../firebase.init";
 import { useForm } from "react-hook-form";
 import Loading from "../Shared/Loading";
 import { Link, useNavigate } from "react-router-dom";
+import useToken from "../../hooks/useToken";
 
 const SignUp = () => {
   const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth);
@@ -19,6 +20,8 @@ const SignUp = () => {
   const [createUserWithEmailAndPassword, user, loading, error] =
     useCreateUserWithEmailAndPassword(auth);
   const [updateProfile, updating, updateError] = useUpdateProfile(auth);
+
+  const [token] = useToken(user || gUser);
 
   const navigate = useNavigate();
 
@@ -38,8 +41,8 @@ const SignUp = () => {
     );
   }
 
-  if (user || gUser) {
-    console.log(user || gUser);
+  if (token) {
+    navigate("/appointment");
   }
 
   const onSubmit = async (data) => {
@@ -47,7 +50,6 @@ const SignUp = () => {
     await createUserWithEmailAndPassword(data.email, data.password);
     await updateProfile({ displayName: data.name });
     console.log("updete done");
-    navigate("/appointment");
   };
   return (
     <div className="flex h-screen justify-center items-center ">
