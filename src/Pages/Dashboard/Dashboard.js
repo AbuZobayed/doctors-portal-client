7f51a61c0d968d554999a1841302faccd9dd61ea
AbuZobayed <zobayed.ict05@gmail.com>
2022-05-18 00:11:06 +0600

    load services with data project and add file type input

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -29,7 +29,10 @@ const Dashboard = () => {
           <li>
             <Link to="/dashboard/history">My History</Link>
           </li>
-          { admin && <li><Link to="/dashboard/users">All Users</Link></li>}
+          { admin && <>
+            <li><Link to="/dashboard/users">All Users</Link></li>
+            <li><Link to="/dashboard/addDoctor">Add a Doctor</Link></li>
+          </>}
           
         </ul>
       </div>
